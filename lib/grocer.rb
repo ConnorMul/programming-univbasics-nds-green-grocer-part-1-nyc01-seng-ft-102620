9a449c4856640634ef8fba6e5c3e7549be416ef9
Connor Mulholland <mulhollandconnor@icloud.com>
2020-10-01 18:44:22 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,7 +9,7 @@ def find_item_by_name_in_collection(name, collection)
      nil
     end 
   end 
-  name ? collection[name][:items] : nil
+  name ? collection[name][:items][:item] : nil
 end
 
 def consolidate_cart(cart)
