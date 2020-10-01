281435a12d54b40defd8c32ca39b8e50bf2fd5ac
Connor Mulholland <mulhollandconnor@icloud.com>
2020-10-01 18:47:58 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,8 +3,8 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
   collection.each do |items|
-    if items[:item] == name
-     return items[:item]
+    items.each do |item_info|
+      if item_info[]
     end 
   end 
 end
